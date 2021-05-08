8066c62b3c9d10406e0221263b7f27aec05274c1
Takayoshi Tanaka <tanaka-takayoshi@users.noreply.github.com>
2021-05-08 12:12:49 +0900

    Added several improvements.

--- NLog.Targets.NewRelicLab.Logs/NewRelicLogsTarget.cs
+++ NLog.Targets.NewRelicLab.Logs/NewRelicLogsTarget.cs
@@ -2,13 +2,14 @@
 using NewRelic.LogEnrichers.NLog;
 using NLog.Common;
 using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets.Wrappers;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
+using System.ComponentModel;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,108 +20,138 @@ namespace NLog.Targets.NewRelicLab.Logs
     {
         private readonly HttpClient client = new HttpClient();
 
-        private string licenseKey;
-        public string LicenseKey 
-        {
-            set
-            {
-                licenseKey = value;
-                client.DefaultRequestHeaders.Remove("X-License-Key");
-                client.DefaultRequestHeaders.Add("X-License-Key", licenseKey);
-            }
-        }
+        public string LicenseKey { get; set; }
 
-        private string insertKey;
-        public string InsertKey
-        { 
-            set
-            {
-                insertKey = value;
-                client.DefaultRequestHeaders.Remove("X-License-Key");
-                client.DefaultRequestHeaders.Add("X-License-Key", insertKey);
-            }
-        }
+        public string InsertKey { get; set; }
 
         public string Endpoint { get; set; }
+
+        [DefaultValue(false)]
+        public bool EscapeJson { get; set; }
 
         public NewRelicLogsTarget()
         {
             Layout = new NewRelicJsonLayout();
             Endpoint = "https://log-api.newrelic.com/log/v1";
-            BatchSize = 10;
-            var licenseKey = Environment.GetEnvironmentVariable("NEW_RELIC_LICENSE_KEY");
-            if (!string.IsNullOrEmpty(licenseKey))
+        }
+
+        protected override Task WriteAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken)
+            => InternalAsyncTask(logEvent, cancellationToken);
+
+        private async Task InternalAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken)
+        {
+            InternalLogger.Debug("Sending one log event");
+            using var sb = ZString.CreateUtf8StringBuilder();
+            sb.Append("[{\"logs\": [");
+            if (EscapeJson)
             {
-                LicenseKey = licenseKey;
-                InternalLogger.Debug("X-License-Key is configured by NEW_RELIC_LICENSE_KEY environment variable.");
+                sb.Append("{\"timestamp\": ");
+                sb.Append(new DateTimeOffset(logEvent.TimeStamp).ToUnixTimeMilliseconds());
+                sb.Append(",\"message\": \"");
+                sb.Append(JsonEncodedText.Encode(Layout.Render(logEvent)));
+                sb.Append("\"}");
             }
             else
             {
-                var insertKey = Environment.GetEnvironmentVariable("NEW_RELIC_INSERT_KEY");
-                if (!string.IsNullOrEmpty(insertKey))
-                {
-                    InsertKey = insertKey;
-                    InternalLogger.Debug("X-Insert-Key is configured by NEW_RELIC_INSERT_KEY environment variable.");
-                }
+                sb.Append(Layout.Render(logEvent));
             }
+            sb.Append("]}]");
+
+            await SendLogStringBuilder(sb, cancellationToken).ConfigureAwait(false);
         }
 
-        protected override Task WriteAsyncTask(LogEventInfo logEvent, CancellationToken cancellationToken) => Task.CompletedTask;
-
-        protected override async Task WriteAsyncTask(IList<LogEventInfo> logEvents, CancellationToken cancellationToken)
+        protected override Task WriteAsyncTask(IList<LogEventInfo> logEvents, CancellationToken cancellationToken)
+            => InternalAsyncTask(logEvents, cancellationToken);
+        
+        private async Task InternalAsyncTask(IList<LogEventInfo> logEvents, CancellationToken cancellationToken)
         {
-            try
+            if (logEvents.Count == 0)
             {
-                if (!logEvents.Any())
-                {
-                    InternalLogger.Debug("log event is empty.");
-                    return;
-                }
+                InternalLogger.Debug("log event is empty.");
+                return;
+            }
 
-                using var sb = ZString.CreateUtf8StringBuilder();
-                sb.Append("[{\"logs\": [");
-                var delimStart = string.Empty;
-                foreach (var logEvent in logEvents)
+            InternalLogger.Debug("# of Sending log events: {0}", logEvents.Count);
+            using var sb = ZString.CreateUtf8StringBuilder();
+            sb.Append("[{\"logs\": [");
+            var delimStart = string.Empty;
+            foreach (var logEvent in logEvents)
+            {
+                sb.Append(delimStart);
+                if (EscapeJson)
                 {
-                    sb.Append(delimStart);
+                    sb.Append("{\"timestamp\": ");
+                    sb.Append(new DateTimeOffset(logEvent.TimeStamp).ToUnixTimeMilliseconds());
+                    sb.Append(",\"message\": \"");
+                    sb.Append(JsonEncodedText.Encode(Layout.Render(logEvent)));
+                    sb.Append("\"}");
+                }
+                else
+                {
                     sb.Append(Layout.Render(logEvent));
-                    delimStart = ",";
                 }
-                sb.Append("]}]");
-                var res = await client.PostAsync(Endpoint, new GZipZStringContent(sb));
-                res.EnsureSuccessStatusCode();
+
+                delimStart = ",";
             }
-            catch (Exception ex)
+            sb.Append("]}]");
+
+            await SendLogStringBuilder(sb, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task SendLogStringBuilder(Utf8ValueStringBuilder sb, CancellationToken cancellationToken)
+        {
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, Endpoint)
             {
-                InternalLogger.Error(ex, "failed to send.");
+                Content = new GZipZStringContent(sb)
+            };
+
+            if (!string.IsNullOrEmpty(LicenseKey))
+            {
+                InternalLogger.Debug("Using license key specified with configuration");
+                requestMessage.Headers.Add("X-License-Key", LicenseKey);
             }
-        }
-    }
-
-    public class GZipZStringContent : HttpContent
-    {
-        private readonly Utf8ValueStringBuilder sb;
-
-        public GZipZStringContent(Utf8ValueStringBuilder sb)
-        {
-            Headers.TryAddWithoutValidation("Content-Type", "application/gzip");
-            Headers.ContentEncoding.Add("gzip");
-            this.sb = sb;
-
-        }
-        protected override Task SerializeToStreamAsync(Stream stream, TransportContext context)
-        {
-            var gzipStream = new GZipStream(stream, CompressionMode.Compress, leaveOpen: true);
-            return sb.WriteToAsync(gzipStream).ContinueWith(_ =>
+            else if (!string.IsNullOrEmpty(InsertKey))
             {
-                gzipStream?.Dispose();
-            });
-        }
+                InternalLogger.Debug("Using insert key specified with configuration");
+                requestMessage.Headers.Add("X-Insert-Key", InsertKey);
+            }
+            else
+            {
+                var lkey = Environment.GetEnvironmentVariable("NEW_RELIC_LICENSE_KEY");
+                if (!string.IsNullOrEmpty(lkey))
+                {
+                    InternalLogger.Debug("Using licenseKey key specified with NEW_RELIC_LICENSE_KEY environment variable.");
+                    requestMessage.Headers.Add("X-License-Key", lkey);
+                }
+                else
+                {
+                    var iKey = Environment.GetEnvironmentVariable("NEW_RELIC_INSERT_KEY");
+                    if (!string.IsNullOrEmpty(iKey))
+                    {
+                        InternalLogger.Debug("Using insert key specified with NEW_RELIC_INSERT_KEY environment variable.");
+                        requestMessage.Headers.Add("X-Insert-Key", iKey);
+                    }
+                    else
+                    {
+                        InternalLogger.Warn("No key found. Abort sending logs.");
+                        return;
+                    }
+                }
+            }
 
-        protected override bool TryComputeLength(out long length)
-        {
-            length = -1L;
-            return false;
+            if (InternalLogger.IsTraceEnabled)
+                InternalLogger.Trace("Sending content: {0}", sb.ToString());
+            var res = await client.SendAsync(requestMessage, cancellationToken).ConfigureAwait(false);
+
+            if (res.IsSuccessStatusCode)
+            {
+                if (InternalLogger.IsTraceEnabled)
+                    InternalLogger.Trace("HTTP POST succeeded: {0}", await res.Content.ReadAsStringAsync().ConfigureAwait(false));
+                return;
+            }
+
+            InternalLogger.Error("Send failed: {0} - {1}", res.StatusCode, await res.Content.ReadAsStringAsync().ConfigureAwait(false));
+            throw new Exception($"HTTP POST failed with {res.StatusCode}.");
         }
     }
 }
--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,7 +1,11 @@
 ﻿using NLog;
 using NLog.Config;
+using NLog.Layouts;
+using NLog.Targets;
 using NLog.Targets.NewRelicLab.Logs;
+using NLog.Targets.Wrappers;
 using System;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
@@ -9,24 +13,36 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            var loggerConfig = new LoggingConfiguration();
+            //NLog.Common.InternalLogger.LogToConsole = true;
+            //NLog.Common.InternalLogger.LogLevel = LogLevel.Trace;
+            //NLog.Common.InternalLogger.LogFile = @"C:\temp\internal.log";
+            //var loggerConfig = new LoggingConfiguration();
 
-            var newRelicTarget = new NewRelicLogsTarget()
-            {
-                LicenseKey = "<Your_NewRelic_LicenseKey>",
-                BatchSize = 10, //default size is 10
-                Endpoint = "https://log-api.newrelic.com/log/v1", //default Endpoint is US
-                // you should not specify Layout. We always use `NewRelicJsonLayout` whatever layout you specify.
-            };
-            loggerConfig.AddRuleForAllLevels(newRelicTarget);
-            LogManager.Configuration = loggerConfig;
+            //var newRelicTarget = new NewRelicLogsTarget()
+            //{
+            //    LicenseKey = "<REPLACE_YOUR_LICENSE_KEY>",
+            //    Endpoint = "https://log-api.newrelic.com/log/v1", //default Endpoint is US
+            //    EscapeJson = true, // If you log message is formatted as JSON, you can forcely formate the JSON to plain formatted string.
+            //    // You can specify all properties of AsyncLogTarget
+            //    BatchSize = 10, 
+            //    RetryCount = 3
+            //    // you don't have to specify Layout. We always use `NewRelicJsonLayout` whatever layout you specify.
+            //};
+            //loggerConfig.AddRuleForAllLevels(newRelicTarget);
+            //loggerConfig.AddRuleForAllLevels(new ConsoleTarget());
+            //LogManager.Configuration = loggerConfig;
+            
             var logger = LogManager.GetLogger("Example");
-            for (int i = 0; i < 100; i++)
+            int i = 0;
+            while (true)
             {
-                logger.Info($"Hello New Relic Logs {i}");
-            }
 
+                logger.Info($"こんにちはNew Relicログ {i++}");
+                Task.Delay(TimeSpan.FromMilliseconds(10)).GetAwaiter().GetResult();
+            }
             Console.ReadLine();
+            LogManager.Shutdown();
+
         }
     }
 }
